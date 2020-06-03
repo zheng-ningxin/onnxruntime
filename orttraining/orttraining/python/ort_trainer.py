449fc192ecf50c6c7519ce55454d39be3b79cff6
suffian khan <sukha@microsoft.com>
2020-06-03 20:21:59 +0000

    correct namespace torch.onnx.TrainingMode

--- orttraining/orttraining/python/ort_trainer.py
+++ orttraining/orttraining/python/ort_trainer.py
@@ -320,7 +320,7 @@ def convert_model_loss_fn_to_onnx(model, loss_fn, model_desc, device, inputs, op
                        output_names=output_names,
                        opset_version=opset_version,
                        dynamic_axes=dynamic_axes,
-                       training=TrainingMode.TRAINING,
+                       training=torch.onnx.TrainingMode.TRAINING,
                        _retain_param_name=True,
                        example_outputs=tuple(sample_outputs),
                        do_constant_folding=False,
