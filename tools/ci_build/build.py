1ed5cc09e21abb14908157476e3681367db3dfc0
suffian khan <sukha@OrtTrainingDev1.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-06-23 04:25:03 +0000

    try another verbose options

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -511,6 +511,7 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home,
     # for now, disable jemalloc if pybind is also enabled.
     cmake_args = [
         cmake_path, cmake_dir,
+        "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON",
         "-Donnxruntime_RUN_ONNX_TESTS=" + (
             "ON" if args.enable_onnx_tests else "OFF"),
         "-Donnxruntime_BUILD_WINML_TESTS=" + (
