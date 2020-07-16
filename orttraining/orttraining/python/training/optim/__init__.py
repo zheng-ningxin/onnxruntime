8b9188710fb37d4c6638d6674bddaf06b6cd1a80
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-07-15 17:24:11 -0700

    TEMPORARY: Fix optim config

--- orttraining/orttraining/python/training/optim/__init__.py
+++ orttraining/orttraining/python/training/optim/__init__.py
@@ -1,4 +1,4 @@
-from .config import AdamConfig, LambConfig, SGDConfig
+from .config import _OptimizerConfig, AdamConfig, LambConfig, SGDConfig
 from .lr_scheduler import _LRScheduler, ConstantWarmupLRScheduler, CosineWarmupLRScheduler,\
     LinearWarmupLRScheduler, PolyWarmupLRScheduler
 
