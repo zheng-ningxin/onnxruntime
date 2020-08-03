321e26f94c240a8e9674cb40811df0b71dc4c532
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2020-08-03 10:47:59 -0700

    Turn error into warning

--- onnxruntime/core/framework/provider_bridge_ort.cc
+++ onnxruntime/core/framework/provider_bridge_ort.cc
@@ -511,7 +511,7 @@ struct ProviderLibrary {
 
 #if defined(_WIN32) && !defined(_OPENMP)
 #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
-#error Unsupported configuration
+#warning Unsupported configuration
 // The following workaround can't be applied in containerized apps (no way to manually load DLLs outside of
 // the packaged app)
 #endif
